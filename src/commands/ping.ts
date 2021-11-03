4500b3c1c7ea4ded0ba8f9aa785f2aeab9f1b8a8
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-11-03 01:54:25 -0600

    Fix the ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,15 +1,19 @@
+import { CommandType, InteractionCallback } from "../constants";
+import { log } from "../main";
+
 const data: Command = {
 	enabled: true,
 
 	structure: {
 		name: `ping`,
 		description: `Ping-Pongs`,
-		type: ApplicationCommandType.ChatInput,
+		type: CommandType.slash,
 	},
 
 	processor(_) {
+		log.info(`Ponging!`);
 		return {
-			type: InteractionCallback.pong,
+			type: InteractionCallback.message_with_source,
 			data: {
 				content: `Pong!`,
 			},
