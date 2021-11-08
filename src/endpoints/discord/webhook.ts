8d8119deb27fe5a1b30570ebc71f41a593731ec9
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-11-08 02:13:02 -0600

    Add generic error handling to the main webhook processor

--- src/endpoints/discord/webhook.ts
+++ src/endpoints/discord/webhook.ts
@@ -28,35 +28,46 @@ export default {
 
 		let data = request.payload as Interaction;
 
-		let event;
-		switch (data.type) {
+		try {
+			let event;
+			switch (data.type) {
 
-			// Discord pinging us to make sure we will respond
-			case InteractionType.Ping:
-				return {
-					type: InteractionResponseType.pong
-				};
+				// Discord pinging us to make sure we will respond
+				case InteractionType.Ping:
+					return {
+						type: InteractionResponseType.pong
+					};
 
-			// Command interaction events (both autocomplete and the final event)
-			case InteractionType.AutoComplete:
-			case InteractionType.ApplicationCommand:
-				event = data.data as CommandPayload;
-				return db.commands[event.name].processor(data);
+				// Command interaction events (both autocomplete and the final event)
+				case InteractionType.AutoComplete:
+				case InteractionType.ApplicationCommand:
+					event = data.data as CommandPayload;
+					return db.commands[event.name].processor(data);
 
-			// Events originating from a button
-			case InteractionType.MessageComponent:
-				event = data.data as ComponentPayload;
-				return db.components[event.custom_id].processor(data);
+				// Events originating from a button
+				case InteractionType.MessageComponent:
+					event = data.data as ComponentPayload;
+					return db.components[event.custom_id].processor(data);
 
-			// Any events that aren't caught
-			default:
-				return {
-					type: 4,
-					data: {
-						content: `How did you trigger this? That should be impossible.`,
-						flags: 1 << 6
-					},
-				};
+				// Any events that aren't caught
+				default:
+					return {
+						type: InteractionResponseType.message_with_source,
+						data: {
+							content: `How did you trigger this? That should be impossible.`,
+							flags: 1 << 6
+						},
+					};
+			};
+		} catch (err) {
+			log.error(`An error occured while processing an interaction`, err);
+			return {
+				type: InteractionResponseType.message_with_source,
+				data: {
+					content: `Something went wrong while processing the interaction.`,
+					flags: 1 << 6,
+				},
+			};
 		};
 	},
 };
