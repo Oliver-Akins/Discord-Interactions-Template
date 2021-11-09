0bb0d1a351dca28b8d99009770261e9776b593fd
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-11-09 00:38:05 -0700

    Remove unneeded DB default now

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,1 @@
 export const discord_api_base = `https://discord.com/api/v9`;
-
-export const default_db = `{
-	"commands": {},
-	"components": {},
-	"raw": {
-		"global": [],
-		"guilds": {}
-	}
-}`
